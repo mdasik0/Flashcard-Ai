514ff70ad92a710e7ef51c0deec166b286c51875
Md asik <asikthe1st@gmail.com>
2025-08-07 09:39:18 +0600

    feat: integrate NextAuth for authentication with Google provider and session management (1/2)

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,31 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+const handler = NextAuth({
+    providers:[
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+        })
+    ],
+    callbacks: {
+        async session({session, token}){
+            if(session.user) {
+                session.user.id = token.sub as string;
+
+            }
+            return session
+        },
+        async jwt({token, user}) {
+            if (user) {
+                token.uid = user.id;
+            }
+            return token;
+        }
+    },
+    pages: {
+        signIn: "/auth/signin",
+    }
+})
+
+export { handler as GET, handler as POST };
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/Navbar/Navbar";
 import { geistMono, geistSans } from "@/lib/fonts";
+import { AuthProvider } from "./providers";
 
 export const metadata: Metadata = {
   title: "Flashcard-Ai",
@@ -20,18 +21,23 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="flex justify-start items-center min-h-screen">
-          <Navbar />
-          <div className="absolute top-10 right-10 ">
-            {user ? 
-            <div className="border border-[#181818] px-5 py-2 rounded-lg uppercase text-sm duration-300 hover:bg-[#181818] cursor-pointer">
-              {name.length > 10 ? `${name.slice(0, 10)}...` : name}
-            </div> : 
-            <div className="border border-[#181818] px-5 py-2 rounded-lg uppercase text-sm duration-300 hover:bg-[#181818] cursor-pointer">
-              Sign in</div>}
+        <AuthProvider>
+          <div className="flex justify-start items-center min-h-screen">
+            <Navbar />
+            <div className="absolute top-10 right-10 ">
+              {user ? (
+                <div className="border border-[#181818] px-5 py-2 rounded-lg uppercase text-sm duration-300 hover:bg-[#181818] cursor-pointer">
+                  {name.length > 10 ? `${name.slice(0, 10)}...` : name}
+                </div>
+              ) : (
+                <div className="border border-[#181818] px-5 py-2 rounded-lg uppercase text-sm duration-300 hover:bg-[#181818] cursor-pointer">
+                  Sign in
+                </div>
+              )}
+            </div>
+            {children}
           </div>
-          {children}
-        </div>
+        </AuthProvider>
       </body>
     </html>
   );
