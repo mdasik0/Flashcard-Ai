da70a1c1cb1de834b1cd0d5c69d2f8bfe2c986d9
Md asik <asikthe1st@gmail.com>
2025-08-31 17:18:51 +0600

    feat: implement MiniCard component and update cards page layout

--- app/cards/page.tsx
+++ app/cards/page.tsx
@@ -1,12 +1,20 @@
-import FlipCard from "@/components/FlipCard/FlipCard";
+import MiniCard from "@/components/mini_card/MiniCard";
 import ProtectedRoute from "@/components/UtilityComp/ProtectedRoute";
 
 export default function cardsPage() {
   return (
     <ProtectedRoute>
       <div className="flex items-center justify-center w-full">
-        {/* whole card */}
-        <FlipCard />
+        <div className="grid grid-cols-6 gap-6">
+          <MiniCard />
+          <MiniCard />
+          <MiniCard />
+          <MiniCard />
+          <MiniCard />
+          <MiniCard />
+          <MiniCard />
+
+        </div>
       </div>
     </ProtectedRoute>
   );
