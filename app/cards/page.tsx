72778fa7a6c5f04305bcddcde1a4a79ffaf98a0b
Md asik <asikthe1st@gmail.com>
2025-10-06 22:19:09 +0600

    feat: add animations and styles for background elements in globals.css

--- app/cards/page.tsx
+++ app/cards/page.tsx
@@ -1,20 +1,11 @@
-import MiniCard from "@/components/mini_card/MiniCard";
+"use client";
 import ProtectedRoute from "@/components/UtilityComp/ProtectedRoute";
 
 export default function cardsPage() {
   return (
     <ProtectedRoute>
-      <div className="flex items-center justify-center w-full">
-        <div className="grid grid-cols-6 gap-6">
-          <MiniCard />
-          <MiniCard />
-          <MiniCard />
-          <MiniCard />
-          <MiniCard />
-          <MiniCard />
-          <MiniCard />
-
-        </div>
+      <div className="h-screen w-[calc(100vw-90px)] flex items-center justify-center">
+        
       </div>
     </ProtectedRoute>
   );
